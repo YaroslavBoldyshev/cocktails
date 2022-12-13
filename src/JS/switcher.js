7cba7057f1dd08732d97e249a09e7b50ab9b573b
vitulya <vitulya.bondarchuk@gmail.com>
2022-12-14 00:31:54 +0200

    update switcher

--- src/JS/switcher.js
+++ src/JS/switcher.js
@@ -1,9 +1,18 @@
 import refs from './refs';
 
+// let theme = localStorage.getItem("theme");
+
 function changeTheme() {
     refs.switcher.addEventListener('change', (event) => {
         refs.body.classList.toggle("dark-theme");
-})
-}
 
+        //     if (document.body.classList.toggle("dark-theme")) {
+        //         return;
+        //     } else {
+        //         localStorage.setItem("theme", "dark-theme");
+        //     }
+        // })
+    })
+}
+    
 changeTheme();
