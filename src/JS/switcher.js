3c5357a2fab5563ae74e0f4d5d83e86d879bd4e1
vitulya <vitulya.bondarchuk@gmail.com>
2022-12-14 00:48:48 +0200

    update switcher-13

--- src/JS/switcher.js
+++ src/JS/switcher.js
@@ -1,18 +1,16 @@
 import refs from './refs';
 
-// let theme = localStorage.getItem("theme");
-
 function changeTheme() {
     refs.switcher.addEventListener('change', (event) => {
         refs.body.classList.toggle("dark-theme");
 
-        //     if (document.body.classList.toggle("dark-theme")) {
-        //         return;
-        //     } else {
-        //         localStorage.setItem("theme", "dark-theme");
-        //     }
-        // })
-    })
-}
-    
+            // if (document.body.classList.toggle("dark-theme")) {
+            //     return;
+            // } else {
+            //     localStorage.setItem("theme", "dark-theme");
+            // }
+        })
+    }
+
+
 changeTheme();
