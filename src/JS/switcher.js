46bf2a1d0824418c1af294bba6326261ab8060bb
vitulya <vitulya.bondarchuk@gmail.com>
2022-12-14 13:49:37 +0200

    change theme on favorite

--- src/JS/switcher.js
+++ src/JS/switcher.js
@@ -1,16 +1,32 @@
 import refs from './refs';
 
+// function changeTheme() {
+//     refs.switcher.addEventListener('change', (event) => {
+//         refs.body.classList.toggle("dark-theme");
+
+//             // if (document.body.classList.toggle("dark-theme")) {
+//             //     return;
+//             // } else {
+//             //     localStorage.setItem("theme", "dark-theme");
+//             // }
+//         })
+//     }
+
+
+// changeTheme();
+
 function changeTheme() {
     refs.switcher.addEventListener('change', (event) => {
         refs.body.classList.toggle("dark-theme");
-
-            // if (document.body.classList.toggle("dark-theme")) {
-            //     return;
-            // } else {
-            //     localStorage.setItem("theme", "dark-theme");
-            // }
+        localStorage.setItem('theme', event.target.checked ? 'dark' : 'ligth')
         })
     }
 
 
 changeTheme();
+
+
+if (localStorage.getItem('theme') === 'dark') {
+    refs.body.classList.add("dark-theme");
+    refs.switcher.checked = true;
+}
